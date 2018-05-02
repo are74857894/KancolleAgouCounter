9a1571f297f7eec227ec766ac5d6f53316c9ac4d
YSRKEN <ysr.ken@gmail.com>
2018-05-02 21:19:27 +0900

    ラベルオブジェクトを全てフィールド変数に移した

--- AgouCounter.java
+++ AgouCounter.java
@@ -21,6 +21,15 @@ public class AgouCounter extends JFrame {
     private JButton btnBossBattleMCount;
     private JButton btnBossButtleReset;
     private JButton btnBossBattleCount;
+    private JLabel kiratuke;
+    private JLabel countLblBoss;
+    private JLabel tasseiBoss;
+    private JLabel countLblBattle;
+    private JLabel tasseiBattle;
+    private JLabel countLblSwin;
+    private JLabel tasseiSwin;
+    private JLabel countLblBossWin;
+    private JLabel tasseiBossWin;
 
 	/**
 	 * Launch the application.
@@ -45,101 +54,101 @@ public class AgouCounter extends JFrame {
 		setContentPane(countSWin);
 		countSWin.setLayout(null);
 
-		JLabel countLblBoss = new JLabel("ボス到達数：0/24");/*ボス到達数*/
+		countLblBoss = new JLabel("ボス到達数：0/24");/*ボス到達数*/
 		countLblBoss.setBounds(17, 105, 118, 16);
 		countSWin.add(countLblBoss);
 
-		JLabel kiratuke= new JLabel("");
+		kiratuke= new JLabel("");
 		kiratuke.setBounds(268, 304, 118, 16);/*キラ付けカウンター用。邪魔だったら消す*/
 		kiratuke.setForeground(Color.BLACK);
 		countSWin.add(kiratuke);
 
-		JLabel tasseiBoss= new JLabel("未達成");
+		tasseiBoss= new JLabel("未達成");
 		tasseiBoss.setBounds(17, 130, 50, 25);/*達成表示用。邪魔だったら消す。*/
 		tasseiBoss.setForeground(Color.BLACK);
 		countSWin.add(tasseiBoss);
 
-		JLabel tasseiBattle = new JLabel("未達成");
+		tasseiBattle = new JLabel("未達成");
 		tasseiBattle.setBounds(17, 65, 45, 16);
 		countSWin.add(tasseiBattle);
 
 		btnBossBattleCount = new JButton("+1");
         btnBossBattleCount.setBounds(147, 95, 50, 60);/*カウンター本体。最低限ここさえあれば*/
-		btnBossBattleCount.addActionListener(e -> clickBtnBossBattleCount(countLblBoss, tasseiBoss));
+		btnBossBattleCount.addActionListener(e -> clickBtnBossBattleCount());
 		countSWin.add(btnBossBattleCount);
 
 		btnBossBattleMCount = new JButton("-1");
 		btnBossBattleMCount.setBounds(206, 95, 50, 60);/*マイナスカウンターのほう。*/
-		btnBossBattleMCount.addActionListener(e -> clickBtnBossBattleMCount(countLblBoss, kiratuke, tasseiBoss));
+		btnBossBattleMCount.addActionListener(e -> clickBtnBossBattleMCount());
 		countSWin.add(btnBossBattleMCount);
 
 		btnBossButtleReset = new JButton("リセット");
 		btnBossButtleReset.setBounds(268, 95, 100, 60);/*リセット処理*/
-		btnBossButtleReset.addActionListener(e -> clickBtnBossButtleReset(countLblBoss, kiratuke, tasseiBoss));
+		btnBossButtleReset.addActionListener(e -> clickBtnBossButtleReset());
 		countSWin.add(btnBossButtleReset);
 
-		JLabel countLblBattle = new JLabel("戦闘数：0/36");/*ここから出撃数カウンター*/
+		countLblBattle = new JLabel("戦闘数：0/36");/*ここから出撃数カウンター*/
 		countLblBattle.setBounds(17, 47, 96, 16);
 		countSWin.add(countLblBattle);
 
 		JButton btnCountBattle = new JButton("+1");
 		btnCountBattle.setBounds(147, 32, 50, 60);
-		btnCountBattle.addActionListener(e -> clickBtnCountBattle(countLblBattle, kiratuke, tasseiBattle));
+		btnCountBattle.addActionListener(e -> clickBtnCountBattle());
 		countSWin.add(btnCountBattle);
 
 		JButton btnCountMBattle = new JButton("-1");
-		btnCountMBattle.addActionListener(e -> clickBtnCountMBattle(countLblBattle, kiratuke, tasseiBattle));
+		btnCountMBattle.addActionListener(e -> clickBtnCountMBattle());
 		btnCountMBattle.setBounds(206, 32, 50, 60);
 		countSWin.add(btnCountMBattle);
 
 		JButton btnBattleReset = new JButton("リセット");
-		btnBattleReset.addActionListener(e -> clickBtnBattleReset(countLblBattle, tasseiBattle));
+		btnBattleReset.addActionListener(e -> clickBtnBattleReset());
 		btnBattleReset.setBounds(268, 32, 100, 60);
 		countSWin.add(btnBattleReset);
 
-		JLabel countLblSwin = new JLabel("S勝利数：0/6");/*ここからS勝利数カウンター*/
+		countLblSwin = new JLabel("S勝利数：0/6");/*ここからS勝利数カウンター*/
 		countLblSwin.setBounds(17, 158, 96, 16);
 		countSWin.add(countLblSwin);
 
-		JLabel tasseiSwin = new JLabel("未達成");
+		tasseiSwin = new JLabel("未達成");
 		tasseiSwin.setBounds(17, 187, 57, 16);
 		countSWin.add(tasseiSwin);
 
 		JButton btnCountSwin = new JButton("+1");
-        btnCountSwin.addActionListener(e -> clickBtnCountSwin(countLblSwin, tasseiSwin));
+        btnCountSwin.addActionListener(e -> clickBtnCountSwin());
 		btnCountSwin.setBounds(147, 158, 50, 60);
 		countSWin.add(btnCountSwin);
 
 		JButton btnCountMSwin = new JButton("-1");
-        btnCountMSwin.addActionListener(e -> clickBtnCountMSwin(countLblSwin, tasseiSwin));
+        btnCountMSwin.addActionListener(e -> clickBtnCountMSwin());
 		btnCountMSwin.setBounds(206, 158, 50, 60);
 		countSWin.add(btnCountMSwin);
 
 		JButton btnSwinReset = new JButton("リセット");
-		btnSwinReset.addActionListener(e -> clickBtnSwinReset(countLblSwin, tasseiSwin));
+		btnSwinReset.addActionListener(e -> clickBtnSwinReset());
 		btnSwinReset.setBounds(268, 158, 100, 60);
 		countSWin.add(btnSwinReset);
 
-		JLabel countLblBossWin = new JLabel("ボス勝利数：0/12");
+		countLblBossWin = new JLabel("ボス勝利数：0/12");
 		countLblBossWin.setBounds(17, 222, 118, 16);
 		countSWin.add(countLblBossWin);
 
-		JLabel tasseiBossWin = new JLabel("未達成");
+		tasseiBossWin = new JLabel("未達成");
 		tasseiBossWin.setBounds(17, 245, 57, 16);
 		countSWin.add(tasseiBossWin);
 
 		JButton btnBossWin = new JButton("+1");/*ここからボス勝利*/
-		btnBossWin.addActionListener(e -> clickBtnBossWin(countLblBossWin, tasseiBossWin));
+		btnBossWin.addActionListener(e -> clickBtnBossWin());
 		btnBossWin.setBounds(147, 223, 50, 60);
 		countSWin.add(btnBossWin);
 
 		JButton btnMBossWin = new JButton("-1");
-		btnMBossWin.addActionListener(e -> clickBtnMBossWin(countLblBossWin, tasseiBossWin));
+		btnMBossWin.addActionListener(e -> clickBtnMBossWin());
 		btnMBossWin.setBounds(206, 223, 50, 60);
 		countSWin.add(btnMBossWin);
 
 		JButton btnBossWinReset = new JButton("リセット");
-		btnBossWinReset.addActionListener(e -> clickBtnBossWinReset(countLblBossWin, tasseiBossWin));
+		btnBossWinReset.addActionListener(e -> clickBtnBossWinReset());
 		btnBossWinReset.setBounds(268, 223, 100, 60);
 		countSWin.add(btnBossWinReset);
 
@@ -150,15 +159,12 @@ public class AgouCounter extends JFrame {
 		countSWin.add(frontCheck);
 
 		JButton btnallReset = new JButton("オールリセット");
-		btnallReset.addActionListener(e -> clickBtnallReset(
-		        countLblBoss, tasseiBoss, countLblBattle, tasseiBattle,
-                countLblSwin, tasseiSwin, countLblBossWin, tasseiBossWin
-        ));
+		btnallReset.addActionListener(e -> clickBtnallReset());
 		btnallReset.setBounds(116, 298, 150, 25);
 		countSWin.add(btnallReset);
 	}
 
-    private void clickBtnBossBattleCount(JLabel countLblBoss, JLabel tasseiBoss){
+    private void clickBtnBossBattleCount(){
         countBossBattle++;
         countLblBoss.setText("ボス到達数："+countBossBattle+"/24");
         if(countBossBattle>=24) {
@@ -169,7 +175,7 @@ public class AgouCounter extends JFrame {
 
         }
     }
-    private void clickBtnBossBattleMCount(JLabel countLblBoss, JLabel kiratuke, JLabel tasseiBoss){
+    private void clickBtnBossBattleMCount(){
         countBossBattle--;
         countLblBoss.setText("ボス到達数："+countBossBattle+"/24");
         if(countBossBattle%3==0) {
@@ -185,14 +191,14 @@ public class AgouCounter extends JFrame {
             }
         }
     }
-    private void clickBtnBossButtleReset(JLabel countLblBoss, JLabel kiratuke, JLabel tasseiBoss){
+    private void clickBtnBossButtleReset(){
         countBossBattle=0;
 
         countLblBoss.setText("ボス到達数："+countBossBattle+"/24");
         tasseiBoss.setText("未達成");
         kiratuke.setText("");
     }
-    private void clickBtnCountBattle(JLabel countLblBattle, JLabel kiratuke, JLabel tasseiBattle){
+    private void clickBtnCountBattle(){
         countBattle++;
         countLblBattle.setText("戦闘数："+countBattle+"/36");
         if(countBattle>=36)
@@ -207,7 +213,7 @@ public class AgouCounter extends JFrame {
 
         }
     }
-    private void clickBtnCountMBattle(JLabel countLblBattle, JLabel kiratuke, JLabel tasseiBattle){
+    private void clickBtnCountMBattle(){
         countBattle--;
         countLblBattle.setText("戦闘数："+countBattle+"/36");
         if(countBattle>=36)
@@ -222,12 +228,12 @@ public class AgouCounter extends JFrame {
             }
         }
     }
-    private void clickBtnBattleReset(JLabel countLblBattle, JLabel tasseiBattle){
+    private void clickBtnBattleReset(){
         countBattle = 0;
         countLblBattle.setText("戦闘数："+countBattle+"/36");
         tasseiBattle.setText("未達成");
     }
-    private void clickBtnCountSwin(JLabel countLblSwin, JLabel tasseiSwin){
+    private void clickBtnCountSwin(){
         countSwin++;
         countLblSwin.setText("S勝利数："+countSwin+"/6");
         if(countSwin>=6)
@@ -236,7 +242,7 @@ public class AgouCounter extends JFrame {
             tasseiSwin.setText("未達成");
         }
     }
-    private void clickBtnCountMSwin(JLabel countLblSwin, JLabel tasseiSwin){
+    private void clickBtnCountMSwin(){
         countSwin--;
         countLblSwin.setText("S勝利数："+countSwin+"/6");
         if(countSwin>=6)
@@ -245,13 +251,13 @@ public class AgouCounter extends JFrame {
             tasseiSwin.setText("未達成");
         }
     }
-    private void clickBtnSwinReset(JLabel countLblSwin, JLabel tasseiSwin){
+    private void clickBtnSwinReset(){
         countSwin=0;
         countLblSwin.setText("S勝利数："+countSwin+"/6");
         if(countSwin<6);
         tasseiSwin.setText("未達成");
     }
-    private void clickBtnBossWin(JLabel countLblBossWin, JLabel tasseiBossWin){
+    private void clickBtnBossWin(){
         countBossWin++;
         countLblBossWin.setText("ボス勝利数："+countBossWin+"/12");
         if(countBossWin>=12);
@@ -260,7 +266,7 @@ public class AgouCounter extends JFrame {
             tasseiBossWin.setText("未達成");
         }
     }
-    private void clickBtnMBossWin(JLabel countLblBossWin, JLabel tasseiBossWin){
+    private void clickBtnMBossWin(){
         countBossWin--;
         countLblBossWin.setText("ボス勝利数："+countBossWin+"/12");
         if(countBossWin>=12);
@@ -269,7 +275,7 @@ public class AgouCounter extends JFrame {
             tasseiBossWin.setText("未達成");
         }
     }
-    private void clickBtnBossWinReset(JLabel countLblBossWin, JLabel tasseiBossWin){
+    private void clickBtnBossWinReset(){
         countBossWin=0;
         countLblBossWin.setText("ボス勝利数："+countBossWin+"/12");
         tasseiBossWin.setText("未達成");
@@ -283,8 +289,7 @@ public class AgouCounter extends JFrame {
         }
         setAlwaysOnTop(top);/*最前面表示*/
     }
-    private void clickBtnallReset(JLabel countLblBoss, JLabel tasseiBoss, JLabel countLblBattle, JLabel tasseiBattle,
-                                  JLabel countLblSwin, JLabel tasseiSwin, JLabel countLblBossWin, JLabel tasseiBossWin){
+    private void clickBtnallReset(){
         countBossBattle=0;
         countBattle=0;
         countBossWin=0;
